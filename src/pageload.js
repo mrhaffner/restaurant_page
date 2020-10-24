503b2957aa72af12b5c02304e82b27e40811532a
Matt Haffner <mrhaffner13@gmail.com>
2020-10-23 23:02:28 -0500

    started adding some style

--- src/pageload.js
+++ src/pageload.js
@@ -7,7 +7,12 @@ const loadPage = () => {
     addText(navBar, 'li', 'home_btn', 'nav_btn', 'Home');
     addText(navBar, 'li', 'menu_btn', 'nav_btn', 'Menu');
     addText(navBar, 'li', 'contact_btn', 'nav_btn', 'Contact');
-    addText(content, 'h1', null, null, 'Future Burger');
+    // const headerDiv = document.createElement('div');
+    // content.appendChild(headerDiv);
+    // headerDiv.id = header_Div;
+    const header = document.createElement('header');
+    content.appendChild(header)
+    addText(header, 'h1', null, null, 'Future Burger');
     content.appendChild(container);
     
 }
