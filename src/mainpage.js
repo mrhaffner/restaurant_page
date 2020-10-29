3a2cc89dad4e3845431d7e3b6492cb1ffe8f7a14
Matt Haffner <mrhaffner13@gmail.com>
2020-10-28 20:31:49 -0500

    added some fonts and created a stylish header

--- src/mainpage.js
+++ src/mainpage.js
@@ -1,6 +1,7 @@
 import { addText, cont } from './createfunction';
 
 const loadMainPage = () => {
+    addText(cont, 'h2', null, null, "Fear The Cheese")
     addText(cont, 'p', null, null, "Future Burger is home of the Jucy Lucy - a burger that is so mouth wateringly delicious that's it's almost as though you were biting into pure flavor crystals.");
     const burgerImage = new Image();
     burgerImage.src = "burger.jpg";
